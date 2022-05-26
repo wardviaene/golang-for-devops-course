3a5e3f90eae97f8c7985ce92b31b5ad0b1d228f0
Edward Viaene <ward@in4it.io>
2022-05-25 21:38:07 -0400

    aws-ec2 code

---
+++ aws-ec2/go.mod
@@ -0,0 +1,20 @@
+module github.com/wardviaene/golang-for-devops-course/aws-ec2
+
+go 1.18
+
+require github.com/aws/aws-sdk-go-v2/config v1.15.8
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.4 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.44.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.6 // indirect
+	github.com/aws/smithy-go v1.11.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ aws-ec2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
+func main() {
+	var (
+		instanceId string
+		err        error
+	)
+	if instanceId, err = createEC2(context.Background()); err != nil {
+		fmt.Printf("Error: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("instance id: %s\n", instanceId)
+}
+
+func createEC2(ctx context.Context) (string, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return "", fmt.Errorf("LoadDefaultConfig error: %s", err)
+	}
+
+	ec2Client := ec2.NewFromConfig(cfg)
+
+	existingKeyPairs, err := ec2Client.DescribeKeyPairs(ctx, &ec2.DescribeKeyPairsInput{
+		KeyNames: []string{"go-aws-ec2"},
+	})
+	if err != nil {
+		return "", fmt.Errorf("DescribeKeyPairs error: %s", err)
+	}
+
+	if len(existingKeyPairs.KeyPairs) == 0 {
+		keyPair, err := ec2Client.CreateKeyPair(ctx, &ec2.CreateKeyPairInput{
+			KeyName: aws.String("go-aws-ec2"),
+		})
+		if err != nil {
+			return "", fmt.Errorf("CreateKeyPair error: %s", err)
+		}
+
+		err = os.WriteFile("go-aws-ec2.pem", []byte(*keyPair.KeyMaterial), 0600)
+		if err != nil {
+			return "", fmt.Errorf("WriteFile (keypair) error: %s", err)
+		}
+	}
+
+	describeImages, err := ec2Client.DescribeImages(ctx, &ec2.DescribeImagesInput{
+		Filters: []types.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: []string{"ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"},
+			},
+			{
+				Name:   aws.String("virtualization-type"),
+				Values: []string{"hvm"},
+			},
+		},
+		Owners: []string{"099720109477"}, // see https://ubuntu.com/server/docs/cloud-images/amazon-ec2
+	})
+	if err != nil {
+		return "", fmt.Errorf("DescribeImages error: %s", err)
+	}
+	if len(describeImages.Images) == 0 {
+		return "", fmt.Errorf("describeImages has empty length (%d)", len(describeImages.Images))
+	}
+	runInstance, err := ec2Client.RunInstances(ctx, &ec2.RunInstancesInput{
+		ImageId:      describeImages.Images[0].ImageId,
+		InstanceType: types.InstanceTypeT3Micro,
+		KeyName:      aws.String("go-aws-ec2"),
+		MinCount:     aws.Int32(1),
+		MaxCount:     aws.Int32(1),
+	})
+	if err != nil {
+		return "", fmt.Errorf("RunInstance error: %s", err)
+	}
+
+	if len(runInstance.Instances) == 0 {
+		return "", fmt.Errorf("RunInstance has empty length (%d)", len(runInstance.Instances))
+	}
+
+	return *runInstance.Instances[0].InstanceId, nil
+}
