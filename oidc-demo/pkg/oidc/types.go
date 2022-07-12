6c46b61f2c38156fcec00d230ed83604466f21d1
Edward Viaene <ward@in4it.io>
2022-07-12 11:45:56 +0200

    add extra attributes to oidc discovery endpoint

--- oidc-demo/pkg/oidc/types.go
+++ oidc-demo/pkg/oidc/types.go
@@ -9,6 +9,9 @@ type Discovery struct {
 	ScopesSupported                   []string `json:"scopes_supported"`
 	ResponseTypesSupported            []string `json:"response_types_supported"`
 	TokenEndpointAuthMethodsSupported []string `json:"token_endpoint_auth_methods_supported"`
+	IDTokenSigningAlgValuesSupported  []string `json:"id_token_signing_alg_values_supported"`
+	ClaimsSupported                   []string `json:"claims_supported"`
+	SubjectTypesSupported             []string `json:"subject_types_supported"`
 }
 
 type Token struct {
